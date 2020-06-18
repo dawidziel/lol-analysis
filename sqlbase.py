f2286f517cf2d59456756c39223236857b49c8aa
Your Name <dawidziel101@gmail.com>
2020-06-18 21:03:39 +0200

    Revert "added some sense"
    This reverts commit 86d49f1a

--- sqlbase.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('API_sql.db')
-cur = conn.cursor()
-cur.execute("insert into summ_list values(NULL,'gazowana kawa')")
-print(cur.fetchall())
-cur.execute('select * from summ_list')
-print(cur.fetchall())
-conn.commit()#fff
