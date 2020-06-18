3477068d3f7e710ef8b758e5db8f7cd448dcc5b3
Your Name <dawidziel101@gmail.com>
2020-06-18 18:26:06 +0200

    added some sense

--- api.py
+++ api.py
@@ -1,6 +1,15 @@
-#start
+import requests
+import json
+def taking_summoner_id(API_KEY,summoner_name, server):
+    url = 'https://' + server +'.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + summoner_name + '?api_key=' + API_KEY
+    answer = requests.get(url)
+    json_answer = json.loads(answer.text)
+    return answer
 def main():
-    print('nic')
+    API_KEY = 'RGAPI-049e494c-77a8-43a3-b711-099b48eec15f'
+    server = input("Podaj nazwę serwera (np. eun1, euw)")
+    summoner_name = input("Podaj nazwę summonera")
+    print(taking_summoner_id(API_KEY,summoner_name,server).content)
 if __name__ == "__main__":
     main()
 
