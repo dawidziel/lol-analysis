5b72cccb6ab51f4739fc79d3e8861dd8623ba533
dywanina <dawidziel101@gmail.com>
2020-06-29 13:50:29 +0200

    new commit

--- api.py
+++ api.py
@@ -1,13 +1,15 @@
-from classes.SummonerController import SummonerController
 from classes.CurrentPlayers import CurrentPlayers
-from classes.StatsController import StatsController
+from classes.TeamStatsController import TeamStatsController
+
+API_KEY = 'RGAPI-87bbf0d7-9a87-40ff-adfc-1de595edb315'
 
 def main():
     try:
         server = input("Podaj nazwę serwera (np. eun1, euw1): ")
-        match_id = input("Podaj matchid: ")
-        stats = StatsController(SummonerController.API_KEY,server, match_id)
-        print(stats.match_stats)
+        match_id = 4674931321
+        stats = StatsController(API_KEY,server, match_id)
+        print(stats.enemy_team_id)
+        print(stats.your_team_id)
         
 
     except:
--- cass.py
+++
@@ -1,6 +0,0 @@
-from riotwatcher import LolWatcher, ApiError
-lol_watcher = LolWatcher('RGAPI-ca3abcba-f2e6-485d-b442-5b641048377d')
-region = 'euw1'
-me = lol_watcher.summoner.by_name(region, 'dywan1na')
-match = lol_watcher.match.by_id(region, 4674931321)
-print(match['participants'][0]['stats']['totalDamageDealtToChampions'])
