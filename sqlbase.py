123109fd058dfd45f447e7bd829270e5733037d3
dywanina <dawidziel101@gmail.com>
2020-06-29 13:54:09 +0200

    new commit

--- sqlbase.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('API_sql.db')
-cur = conn.cursor()
-siwas = "k3w1s"
-cur.execute('''insert into summ_list(summ_name) values(?)''', (siwas,))
-print(cur.fetchall())
-cur.execute('select * from summ_list')
-print(cur.fetchall())
-conn.commit()
