a1844f562ad7ed0c4a5a0910ad7ce3a0ef87ac02
dywanina <dawidziel101@gmail.com>
2020-06-30 23:16:44 +0200

    added new function and organized the look of it

--- api.py
+++ api.py
@@ -1,15 +1,17 @@
 from classes.CurrentPlayers import CurrentPlayers
 from classes.TeamStatsController import TeamStatsController
+from Keys import Keys
 
-API_KEY = 'RGAPI-87bbf0d7-9a87-40ff-adfc-1de595edb315'
+API_KEY = Keys.API_KEY
+match_id = Keys.match_id
 
 def main():
     try:
         server = input("Podaj nazwę serwera (np. eun1, euw1): ")
-        match_id = 4674931321
-        stats = StatsController(API_KEY,server, match_id)
-        print(stats.enemy_team_id)
-        print(stats.your_team_id)
+        team_stats = TeamStatsController(API_KEY,server, match_id)
+        print(team_stats.enemy_team_id)
+        print(team_stats.taking_team_stats())
+        print("\n" + team_stats.list_enemy_champions)
         
 
     except:
--- classes/TeamStatsController.py
+++ classes/TeamStatsController.py
@@ -20,12 +20,14 @@ class TeamStatsController:
 
 #function that diagnoses which team_id is suited for your team   
     def diagnosing_team_id(self):
+        list_your_champions = []
         id_list = [100,200]
         for i in id_list:
             champion_ids = [str(x['championId']) for x in self.match['participants'] if x['teamId'] == i]
             for x in self.champions:
                 if self.champions[x]['key'] in champion_ids:
-                    print(self.champions[x]['id'])
+                    list_your_champions.append(self.champions[x]['id'])
+            print(list_your_champions)
             question = input("Was it your teamcomp? (y/n)")
             if question == "y":
                 self.your_team_id = i
@@ -33,17 +35,21 @@ class TeamStatsController:
                 self.enemy_team_id = id_list[0]
                 break
             elif question == "n":
+                list_your_champions.clear()
                 continue
             else:
                 print("Please input correct charakter")
                 break
-           # elif question == "n":
-           #     continue
-            #else:
-             #   print("Please input correct value")
 
+#diagnosing which champions are being played by which player
+#    def diagnosing_
         
 
 #below function takes overall team stats
-#    def taking_match_stats(self):
-#        match1 = [str(x['championId']) for x in self.match['participants'] if x['teamId'] == 200]
+    def taking_team_stats(self):
+        team_stats = [x for x in self.match['teams'] if x['teamId'] == self.your_team_id]
+        return team_stats
+
+#function takes individual stats
+#    def taking_player_stats(self):
+
